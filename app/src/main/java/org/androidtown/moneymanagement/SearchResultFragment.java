233267360f283ebee810389fb72151c02e160085
2017029425 <sunung007@hanyang.ac.kr>
2018-11-24 21:47:47 +0900

    result

--- app/src/main/java/org/androidtown/moneymanagement/SearchResultFragment.java
+++ app/src/main/java/org/androidtown/moneymanagement/SearchResultFragment.java
@@ -1,25 +1,23 @@
 package org.androidtown.moneymanagement;
 
+import android.content.Context;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Spinner;
-import android.widget.Toast;
 
 
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link SearchStudentFragment.OnFragmentInteractionListener} interface
+ * {@link SearchResultFragment.OnFragmentInteractionListener} interface
  * to handle interaction events.
- * Use the {@link SearchStudentFragment#newInstance} factory method to
+ * Use the {@link SearchResultFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class SearchStudentFragment extends Fragment {
+public class SearchResultFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -29,15 +27,9 @@ public class SearchStudentFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    private Spinner mSidView;
-    private EditText mSnameView;
-//    private View mProgressView;
-    private View mSearchView;
-
-
 //    private OnFragmentInteractionListener mListener;
 
-    public SearchStudentFragment() {
+    public SearchResultFragment() {
         // Required empty public constructor
     }
 
@@ -47,11 +39,11 @@ public class SearchStudentFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment SearchStudentFragment.
+     * @return A new instance of fragment SearchResultFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static SearchStudentFragment newInstance(String param1, String param2) {
-        SearchStudentFragment fragment = new SearchStudentFragment();
+    public static SearchResultFragment newInstance(String param1, String param2) {
+        SearchResultFragment fragment = new SearchResultFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -71,30 +63,16 @@ public class SearchStudentFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
-        View view = inflater.inflate(R.layout.fragment_search_student, container, false);
-
-        Button studentSearchButton = (Button) view.findViewById(R.id.student_search_button);
-        studentSearchButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(view.getContext(), "Clicked", Toast.LENGTH_LONG);
-//                searchStudent();
-            }
-        });
-
-        mSearchView = view.findViewById(R.id.search_form);
-//        mProgressView = findViewById(R.id.search_progress);
-
-
-        return view;
-
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_search_result, container, false);
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed() {
-        Toast.makeText(getActivity(), "Clicked", Toast.LENGTH_LONG);
-    }
+//    // TODO: Rename method, update argument and hook method into UI event
+//    public void onButtonPressed(Uri uri) {
+//        if (mListener != null) {
+//            mListener.onFragmentInteraction(uri);
+//        }
+//    }
 //
 //    @Override
 //    public void onAttach(Context context) {
@@ -127,37 +105,4 @@ public class SearchStudentFragment extends Fragment {
 //        // TODO: Update argument type and name
 //        void onFragmentInteraction(Uri uri);
 //    }
-
-    /*
-    private void searchStudent() {
-        // Reset errors.
-        mSnameView.setError(null);
-
-        // Store values at the time of the search attempt.
-        String sid = mSidView.getSelectedItem().toString();
-        String sname = mSnameView.getText().toString();
-
-        boolean cancel = false;
-        View focusView = null;
-
-        // Check for a nonempty name.
-        if (TextUtils.isEmpty(sid)) {
-            mSnameView.setError(getString(R.string.error_field_required));
-            focusView = mSnameView;
-            cancel = true;
-        }
-
-        if (cancel) {
-            // There was an error; don't attempt login and focus the first
-            // form field with an error.
-            focusView.requestFocus();
-        } else {
-            // Show a progress spinner, and kick off a background task to
-            // perform the search attempt.
-            showProgress(true);
-            mAuthTask = new Students(sid, sname);
-            mAuthTask.execute((Void) null);
-        }
-    }
-*/
 }
